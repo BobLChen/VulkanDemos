14945d2a75ea532840d7703df89416d295b68e8c
BobLCheno <chenbo150928@gmail.com>
2019-03-14 20:25:42 +0800

    添加Android构建选项

---
+++ Android/examples/triangle/triangle.cpp
@@ -0,0 +1,20 @@
+#include <jni.h>
+
+#include <errno.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <sys/resource.h>
+#include <android/native_activity.h>
+#include <android/asset_manager.h>
+#include <android_native_app_glue.h>
+#include <sys/system_properties.h>
+#include <android/log.h>
+
+#define LOGI(...) ((void)__android_log_print(ANDROID_LOG_INFO, "threaded_app", __VA_ARGS__))
+#define LOGE(...) ((void)__android_log_print(ANDROID_LOG_ERROR, "threaded_app", __VA_ARGS__))
+
+void android_main(android_app* state)
+{
+    LOGE("chu shi hua chengong 测试完成哟呵 ................................\n");
+}
