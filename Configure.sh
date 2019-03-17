3cde497042b338d70f62df0060fdd0e4d3d34223
BobLChen <chenbo150928@gmail.com>
2019-03-17 16:34:23 +0800

    ubuntu环境搭建教程

--- Configure.sh
+++ Configure.sh
@@ -8,4 +8,4 @@ cd build
 cd Debug
 Local_Dir=$(cd "$(dirname "$0")"; pwd)
 echo "Now work at Dir:$Local_Dir"
-cmake ../.. -DCMAKE_BUILD_TYPE=Debug -DGLFW_BUILD_EXAMPLES=OFF -DGLFW_BUILD_TESTS=OFF -DGLFW_BUILD_DOCS=OFF -DGLFW_INSTALL=OFF
+cmake ../.. -DCMAKE_BUILD_TYPE=Debug
