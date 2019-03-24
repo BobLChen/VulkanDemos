962e32b91147cc80c76f0f9bf4fc8c4b54cca3bd
BobLChen <chenbo150928@gmail.com>
2019-03-24 16:19:50 +0800

    修复bug

--- examples/2_Triangle/Triangle.cpp
+++ examples/2_Triangle/Triangle.cpp
@@ -182,8 +182,8 @@ private:
 		clearValues[0].color        = { {0.2f, 0.2f, 0.2f, 1.0f} };
 		clearValues[1].depthStencil = { 1.0f, 0 };
 
-		uint32 width = m_VulkanRHI->GetSwapChain()->GetWidth();
-		uint32 height = m_VulkanRHI->GetSwapChain()->GetHeight();
+		int32 width = m_VulkanRHI->GetSwapChain()->GetWidth();
+		int32 height = m_VulkanRHI->GetSwapChain()->GetHeight();
 
 		VkRenderPassBeginInfo renderPassBeginInfo;
 		ZeroVulkanStruct(renderPassBeginInfo, VK_STRUCTURE_TYPE_RENDER_PASS_BEGIN_INFO);
