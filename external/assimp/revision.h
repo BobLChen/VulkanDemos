5cb80950b87d492c18ec2ad365d8adde4bc52780
BobLChen <chenbo150928@gmail.com>
2019-05-23 21:51:48 +0800

    1、修复构建错误 2、修改examle名称

--- external/assimp/revision.h
+++ external/assimp/revision.h
@@ -1,7 +1,7 @@
 #ifndef ASSIMP_REVISION_H_INC
 #define ASSIMP_REVISION_H_INC
 
-#define GitVersion 0x97587606
-#define GitBranch "dev"
+#define GitVersion 0x3db4b187
+#define GitBranch "master"
 
 #endif // ASSIMP_REVISION_H_INC
