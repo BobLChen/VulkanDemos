5a3c7a3df6f3798c68274154260ab63c8debd9c6
BobLCheno <chenbo150928@gmail.com>
2019-05-29 18:26:42 +0800

    1、添加texture array demo 2、assimp只解析fbx、glb、obj格式

--- external/assimp/revision.h
+++ external/assimp/revision.h
@@ -1,7 +1,7 @@
 #ifndef ASSIMP_REVISION_H_INC
 #define ASSIMP_REVISION_H_INC
 
-#define GitVersion 0x97587606
+#define GitVersion 0x3db4b187
 #define GitBranch "dev"
 
 #endif // ASSIMP_REVISION_H_INC
