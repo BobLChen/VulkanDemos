798823c793c57a4909a72d1bf323821331f8f812
BobLCheno <chenbo150928@gmail.com>
2019-07-12 12:27:41 +0800

    临时屏蔽时间

--- Engine/Monkey/GenericPlatform/GenericPlatformTime.h
+++ Engine/Monkey/GenericPlatform/GenericPlatformTime.h
@@ -2,15 +2,27 @@
 
 #include "Common/Common.h"
 
-#include <sys/time.h>
+#if !PLATFORM_WINDOWS
+	#include <sys/time.h>
+#endif
+
 #include <cstddef>
 
 struct GenericPlatformTime
 {
+
+#if !PLATFORM_WINDOWS
     static FORCEINLINE double Seconds()
 	{
 		struct timeval tv;
 		gettimeofday(&tv, NULL);
 		return ((double) tv.tv_sec) + (((double) tv.tv_usec) / 1000000.0);
 	}
+#else
+	static FORCEINLINE double Seconds()
+	{
+		return 0.0;
+	}
+#endif
+
 };
