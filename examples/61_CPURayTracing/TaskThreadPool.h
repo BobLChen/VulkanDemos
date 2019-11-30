1a3abfcdd3c24a2f4a3991932e4b208c9090d89d
BobLChen <chenbo150928@gmail.com>
2019-11-30 22:25:49 +0800

    thread task

---
+++ examples/61_CPURayTracing/TaskThreadPool.h
@@ -0,0 +1,51 @@
+﻿#pragma once
+
+#include "Common/Common.h"
+
+#include <vector>
+#include <mutex>
+#include <condition_variable>
+
+class TaskThread;
+class ThreadTask;
+
+class TaskThreadPool
+{
+public:
+
+	TaskThreadPool();
+
+	virtual ~TaskThreadPool();
+
+	virtual bool Create(uint32 numThreads);
+
+	virtual void Destroy();
+
+	virtual void AddTask(ThreadTask* task);
+
+	virtual bool RetractTask(ThreadTask* task);
+
+	virtual ThreadTask* ReturnToPoolOrGetNextJob(TaskThread* thread);
+
+	static TaskThreadPool* Allocate();
+
+	int32 GetNumQueuedJobs() const
+	{
+		return m_QueuedTask.size();
+	}
+
+	int32 GetNumThreads() const
+	{
+		return m_AllThreads.size();
+	}
+
+protected:
+
+	std::vector<ThreadTask*>		m_QueuedTask;
+	std::vector<TaskThread*>		m_QueuedThreads;
+	std::vector<TaskThread*>		m_AllThreads;
+
+	std::mutex						m_SynchMutex;
+	bool							m_TimeToDie = false;
+
+};
---
+++ examples/61_CPURayTracing/ThreadEvent.h
@@ -0,0 +1,73 @@
+﻿#pragma once
+
+#include "Common/Common.h"
+
+#include <mutex>
+#include <condition_variable>
+
+class ThreadEvent
+{
+public:
+	enum TriggerType
+	{
+		TRIGGERED_NONE,
+		TRIGGERED_ONE,
+		TRIGGERED_ALL,
+	};
+
+public:
+
+	ThreadEvent(bool isManualReset = false);
+
+	virtual ~ThreadEvent();
+
+	void Trigger();
+
+	void Reset();
+
+	bool Wait(uint32 waitTime = (uint32)-1);
+
+	bool IsInitialized()
+	{
+		return m_Initialized;
+	}
+
+	bool IsManualReset()
+	{
+		return m_IsManualReset;
+	}
+
+	int32 WaitingThreads()
+	{
+		return m_WaitingThreads;
+	}
+
+	bool Wait()
+	{
+		return Wait((uint32)-1);
+	}
+
+private:
+
+	FORCEINLINE void Lock()
+	{
+		m_Mutex.lock();
+	}
+
+	FORCEINLINE void Unlock()
+	{
+		m_Mutex.unlock();
+	}
+
+private:
+
+	bool					m_Initialized;
+	bool					m_IsManualReset;
+
+	volatile TriggerType	m_Triggered;
+	volatile int32			m_WaitingThreads;
+
+	std::mutex				m_Mutex;
+	std::condition_variable	m_Condition;
+
+};
